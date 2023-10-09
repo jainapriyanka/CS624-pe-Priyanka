9f11f6c6fbb83df7101bd51b3579d0d0ebcf8959
jainapriyanka <147120582+jainapriyanka@users.noreply.github.com>
2023-10-09 02:36:36 +0000

    Created a Hello World Mobile App

---
+++ my-mobile-app/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Priyanka Jaina</Text>
+      <Text>MSCS</Text>
+      <Text>CityU</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
